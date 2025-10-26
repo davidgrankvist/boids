093b56f56be810f5d433825c10d863975e2ac694
David Grankvist <grankvist.david@gmail.com>
2025-10-26 14:40:17 +0100

    add simulation

--- src/boids.h
+++ src/boids.h
@@ -7,23 +7,32 @@ struct Boid
 {
     Vector2 position;
     Vector2 velocity;
-    float speed;
-    float acceleration;
     Vector2 size;
-
     Color color;
 };
 
-struct WorldBounds 
+struct SimulationSettings 
 {
+    int maxBoids;
     Vector2 boundsMin;
     Vector2 boundsMax;
+
+    float visibleRange;
+    float protectiveRange;
+    float avoidFactor;
+    float matchingFactor;
+    float centeringFactor;
+    float turnFactor;
+
+    float minSpeed;
+    float maxSpeed;
 };
 
 struct BoidState 
 {
     std::vector<Boid> boids;
-    WorldBounds bounds;
+    std::vector<Boid> prevBoids;
+    SimulationSettings settings;
 };
 
 #endif
